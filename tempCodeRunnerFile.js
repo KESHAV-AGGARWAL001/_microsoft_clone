449461288ca50e89fdd44489f7fea00167d84126
keshav <aggarwalkeshav001@gmial.com>
2022-07-01 08:05:52 +0530

    i have added my microsoft clone here

---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,8 @@
+
+// var boolean = true;
+// var myAge = 34; 
+// var $myName = "   thapa technical";
+// console.log(typeof(undefined));
+// console.log(typeof(boolean));
+// console.log(typeof(myAge));
+// console.log(typeof($myN
